555246eaf0616c541c4e66eed1ee99b339c68f3b
Jonathan Sagan <sagan.jonathan@gmail.com>
2020-04-08 15:36:20 +0200

    TP1.1

---
+++ src/main/java/dev/AppSpringXML.java
@@ -0,0 +1,34 @@
+package dev;
+
+import java.util.Scanner;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import dev.ihm.Menu;
+
+/**
+ * Création du contexte Spring
+ * 
+ * @author Jonathan
+ *
+ */
+public class AppSpringXML {
+	public static void main(String[] args) {
+		// creation contexte Spring config mémoire
+		//ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-config-memoire.xml");
+		// creation contexte Spring config fichier
+				ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-config-fichier.xml");
+		// récupération du bean Scanner
+		Scanner scanner = context.getBean(Scanner.class);
+		
+		// récupération du bean Menu
+		Menu menu = context.getBean(Menu.class);
+		menu.afficher();
+		
+		// fermeture du Scanner
+		context.getBean(Scanner.class).close();
+		
+		// fermeture du contexte Spring
+		context.close();
+	}
+}
